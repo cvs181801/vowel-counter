b8a2e302867f3a465921f141ccf1d1a4818784d6
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-12 11:05:31 -0700

    try again

--- index.js
+++ index.js
@@ -26,12 +26,13 @@ function countVowelsIterative(textValue) {
     
 }
 
+countVowelsIterative(textValue);
 
  //create an event listener that starts the vowel count when the user clicks on the 'go' button.
   goButton.addEventListener('click',
      countVowelsIterative());
 
-countVowelsIterative(textValue);
+
 
 
 
