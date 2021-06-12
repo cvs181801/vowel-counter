00af46feafdf161c8f2efe645de82daf985f963c
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-12 14:27:44 -0700

    move the .value to function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let counter = 0;
 
 //grab the elements
   const goButton = document.getElementById("go-btn");
-  const textValue = document.getElementById("textBox").value;
+  const textValue = document.getElementById("textBox");
   const parentDiv = document.getElementById("resultDiv");
   const renderCounter = document.createElement('p');
 
@@ -30,7 +30,7 @@ countVowelsIterative(textValue);
 
  //create an event listener that starts the vowel count when the user clicks on the 'go' button.
   goButton.addEventListener("click", function() {
-      return countVowelsIterative(textValue)
+      return countVowelsIterative(textValue.value);
   });
   
 //countVowelsIterative(textValue);
