0330c323f82de97f91fd7a412bce2ea8b6976a2a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-11 20:10:33 -0700

    checking in the console

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
-
+//define the vowels
 const vowels = ["a", "e", "i", "o", "u"]
 
+//create a way to check each letter of the inserted text for vowels
 function countVowelsIterative(text) {
     let counter = 0;
     for (let letter of text.toLowerCase()) {
@@ -9,5 +10,12 @@ function countVowelsIterative(text) {
             counter++;
         }
     }
+    console.log(`Volume: ${counter}`)
+    return counter;
 }
 
+//create an event listener that starts the vowel count when the user clicks on the 'go' button.
+
+countVowelsIterative()
+
+console.log("happy");
