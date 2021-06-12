200f0b70768631bdeb4fce2acfac08e4b9bec73f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-12 11:11:07 -0700

    test event listener

--- index.js
+++ index.js
@@ -30,9 +30,11 @@ countVowelsIterative(textValue);
 
  //create an event listener that starts the vowel count when the user clicks on the 'go' button.
   goButton.addEventListener('click', function() {
-     return countVowelsIterative(textValue)});
+     //return countVowelsIterative(textValue)
+     console.log("hoopla");
+    });
 
-countVowelsIterative(textValue);
+//countVowelsIterative(textValue);
 
 
 
