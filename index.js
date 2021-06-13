2a172705a4e19599d4c0bd592fc46a46ef3e6c6b
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-13 10:11:19 -0700

    re do loop

--- index.js
+++ index.js
@@ -1,55 +1,68 @@
 
-//define the vowels, and start counter at 0
-const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-let counter = 0;
+// //define the vowels, and start counter at 0
+ const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+ let counter = 0;
 
-//grab the elements
-  const goButton = document.getElementById("go-btn");
-  const textValue = document.getElementById("textBox");
-  const parentDiv = document.getElementById("resultDiv");
-  const renderCounter = document.createElement('p');
+// //grab the elements
+//   const goButton = document.getElementById("go-btn");
+//   const textValue = document.getElementById("textBox");
+//   const parentDiv = document.getElementById("resultDiv");
+//   const renderCounter = document.createElement('p');
 
-  textValueArr = [];
+//   textValueArr = [];
 
 
-//create a way to count the vowels in the text the user types in
+function countVowelsIterative(text) {
+  let counter = 0
+
+  for (let letter of text) {
+    if (vowels.includes(letter)) {
+      counter++
+    }
+  }
+
+  console.log(`The text contains ${counter} vowel(s)`)
+
+  return counter
+}
+// //create a way to count the vowels in the text the user types in
+// // function countVowelsIterative(text) {
+// //     let counter = 0;
+// //     textValueArr.push(textValue.value); 
+// //     for (let letter of textValueArr) {
+// //         if (textValueArr.includes(letter)) {
+// //             counter++;
+// //         }  
+// //     }
+    
+// //     renderCounter.innerText = '';
+// //     renderCounter.innerText = `Number of Vowels: ${counter}`;
+// //     parentDiv.append(renderCounter);
+// //     return counter;
+    
+// // }
+
+// **I rerwrote the above function using a while loop instead.  I haven't gotten it to work yet **
 // function countVowelsIterative(text) {
 //     let counter = 0;
-//     textValueArr.push(textValue.value); 
-//     for (let letter of textValueArr) {
-//         if (textValueArr.includes(letter)) {
-//             counter++;
-//         }  
+//     textValueArr.push(textValue.value);
+//     while (textValueArr.length) {
+//         counter++;
 //     }
-    
-//     renderCounter.innerText = '';
-//     renderCounter.innerText = `Number of Vowels: ${counter}`;
-//     parentDiv.append(renderCounter);
-//     return counter;
-    
+//      renderCounter.innerText = '';
+//      renderCounter.innerText = `Number of Vowels: ${counter}`;
+//      parentDiv.append(renderCounter);
+//      return counter;
 // }
 
-//I rerwrote the above function using a while loop instead.
-function countVowelsIterative(text) {
-    let counter = 0;
-    textValueArr.push(textValue.value);
-    while (textValueArr.length) {
-        counter++;
-    }
-     renderCounter.innerText = '';
-     renderCounter.innerText = `Number of Vowels: ${counter}`;
-     parentDiv.append(renderCounter);
-     return counter;
-}
+// countVowelsIterative(textValue);
 
-countVowelsIterative(textValue);
-
- //create an event listener that starts the vowel count when the user clicks on the 'go' button.
-  goButton.addEventListener("click", function() {
-      return countVowelsIterative(textValue.value);
-  });
+//  //create an event listener that starts the vowel count when the user clicks on the 'go' button.
+//   goButton.addEventListener("click", function() {
+//       return countVowelsIterative(textValue.value);
+//   });
   
-//countVowelsIterative(textValue);
+// //countVowelsIterative(textValue);
 
 
 
