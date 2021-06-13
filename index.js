6a5c19ef0b24b79f86ead0fb6811f3393b5298f1
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-13 10:45:42 -0700

    find data type

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 // //grab the elements
 //   const goButton = document.getElementById("go-btn");
-   const textValue = document.getElementById("textBox");
+   const textValue = document.getElementById("textBox").value;
 //   const parentDiv = document.getElementById("resultDiv");
 //   const renderCounter = document.createElement('p');
 
