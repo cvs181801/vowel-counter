8c55ef7337b85e8c23bb4838da9c1fbddc3260d9
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-13 10:34:11 -0700

    try adding event listener back in

--- index.js
+++ index.js
@@ -72,11 +72,11 @@ function countVowelsIterative(text) {
 //countVowelsIterative(textValue);
 
  //create an event listener that starts the vowel count when the user clicks on the 'go' button.
-//   goButton.addEventListener("click", function() {
-//       return countVowelsIterative(textValue.value);
-//   });
+   goButton.addEventListener("click", function() {
+       return countVowelsIterative(textValue.value);
+   });
   
-countVowelsIterative(textValue.value);
+//countVowelsIterative(textValue.value);
 
 
 
