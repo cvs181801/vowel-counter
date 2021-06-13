ceca3bf604c70b63c923a1e2795ccb5d2a9f66d3
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-13 10:26:52 -0700

    try again with a for...of loop

--- index.js
+++ index.js
@@ -3,15 +3,28 @@
  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
  let counter = 0;
 
-// //grab the elements
-//   const goButton = document.getElementById("go-btn");
-   const textValue = document.getElementById("textBox");
-//   const parentDiv = document.getElementById("resultDiv");
-//   const renderCounter = document.createElement('p');
+//grab the elements
+  const goButton = document.getElementById("go-btn");
+  const textValue = document.getElementById("textBox");
+  const parentDiv = document.getElementById("resultDiv");
+  const renderCounter = document.createElement('p');
 
-let textValueArr = [];
-textValueArr = [textValue.value];
-console.log(textValueArr);
+//let textValueArr = [];
+
+function countVowelsIterative(text) {
+    let counter = 0;
+  
+    for (let letter of text) {
+      if (vowels.includes(letter)) {
+        counter++
+      }
+    }
+  
+    console.log(`The text contains ${counter} vowel(s)`)
+  
+    return counter
+  }
+
 
 // function countVowelsIterative(textValueArr) {
 //   let counter = 0
@@ -56,14 +69,14 @@ console.log(textValueArr);
 //      return counter;
 // }
 
-// countVowelsIterative(textValue);
+//countVowelsIterative(textValue);
 
-//  //create an event listener that starts the vowel count when the user clicks on the 'go' button.
+ //create an event listener that starts the vowel count when the user clicks on the 'go' button.
 //   goButton.addEventListener("click", function() {
 //       return countVowelsIterative(textValue.value);
 //   });
   
-//countVowelsIterative();
+countVowelsIterative();
 
 
 
